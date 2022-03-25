d72ec72d3316f20f9ef6524689867f468cdc56cd
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-25 14:24:52 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
 import { Link } from 'gatsby'
 
 const IndexPage: FunctionComponent = () => {
   return (
     <>
-      <Text text="Home" />
       <Link to='/info/'>To info</Link>
     </>
   )
