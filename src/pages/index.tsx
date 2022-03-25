d35de2dfe463fbb9a6100c44730403df21849785
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-25 13:09:54 +0900

    Add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Text'
+
+const IndexPage: FunctionComponent = () => {
+  return <Text text="Home"/>
+}
+
+export default IndexPage;
