e7fa9585389984f9262809df2d6d8be0657abbcd
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-25 13:30:48 +0900

    Add anchor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,12 @@ import React, { FunctionComponent } from 'react'
 import Text from 'components/Text'
 
 const IndexPage: FunctionComponent = () => {
-  return <Text text="Home"/>
+  return (
+    <>
+      <Text text="Home" />
+      <a href='./info/'>To info</a>
+    </>
+  )
 }
 
 export default IndexPage;
