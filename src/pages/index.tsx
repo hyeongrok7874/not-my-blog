9b1252aba81bbb147f9825ed4d6faa4c174cd36d
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-25 13:34:17 +0900

    Add Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent } from 'react'
 import Text from 'components/Text'
+import { Link } from 'gatsby'
 
 const IndexPage: FunctionComponent = () => {
   return (
     <>
       <Text text="Home" />
-      <a href='./info/'>To info</a>
+      <Link to='/info/'>To info</Link>
     </>
   )
 }
