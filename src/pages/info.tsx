3fdf5ae588fc5e25961c9258d70ecd9bc3af42c6
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-27 18:59:25 +0900

    Add global style

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,16 @@
 import React, {FunctionComponent} from "react";
 import { graphql } from "gatsby";
+import styled from "@emotion/styled";
 import { Global, css } from "@emotion/react"
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 20px;
+  }
+`
 
 type InfoPageProps = {
   data : {
@@ -23,6 +33,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = ({
 }) => {
   return (
     <div>
+      <Global styles={globalStyle} />
       {title} {description} {author}
     </div>
   )
