651202a8d98977219d413372735facd5a044e4ca
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-27 22:17:38 +0900

    Add styled components props

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -18,14 +18,16 @@ const TextStyle = css`
   color: gray;
 `
 
-const Text1 = styled.div`
+const Text1 = styled.div<{ disable: boolean }>`
   font-size: 20px;
   font-weight: 700;
+  text-decoration: ${({disable}) => (disable ? 'line-through' : 'none')} ;
 `
 
-const Text2 = styled('div')(() => ({
+const Text2 = styled('div')<{ disable: boolean }>(({disable}) => ({
   fontSize: '15px',
   color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
 }))
 
 type InfoPageProps = {
@@ -51,8 +53,8 @@ const InfoPage: FunctionComponent<InfoPageProps> = ({
     <div>
       <Global styles={globalStyle} />
       <div css={TextStyle}>{title}</div>
-      <Text1>{description}</Text1>
-      <Text2>{author}</Text2>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
     </div>
   )
 }
