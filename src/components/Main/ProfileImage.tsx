f91c64a42e217dad2c37a2f45c6778e5137850e6
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-28 23:35:44 +0900

    Add profileImage

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -8,8 +8,10 @@ const ProfileImageWrapper = styled.img`
   border-radius: 50%;
 `
 
+const PROFILE_IMAGE_LINK = 'https://avatars.githubusercontent.com/u/80103328?v=4'
+
 const ProfileImage: FunctionComponent = () => {
-  return <ProfileImageWrapper src="" alt="Profile Image" />
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
 }
 
 export default ProfileImage
