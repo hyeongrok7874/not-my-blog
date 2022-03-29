092aa8820b40e971950d340c1f9b5a1bb1b58117
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-29 21:11:34 +0900

    Add Footer

---
+++ src/components/Common/Footer/style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const FooterWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+`
