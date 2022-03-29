1f5f7c65855d8c70c74940c22ce51ef399d4598b
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-29 18:03:15 +0900

    Add Introduction style

---
+++ src/components/Main/Introduction/style.ts
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const Background = styled.div`
+  width: 100%;
+  background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+  color: #ffffff;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 768px;
+  height: 400px;
+  margin: 0 auto;
+`
+
+export const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+`
+
+export const Title = styled.div`
+  margin-top: 5px;
+  font-size: 35px;
+  font-weight: 700;
+`
