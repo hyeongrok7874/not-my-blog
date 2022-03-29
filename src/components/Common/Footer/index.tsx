cc23c12aff723e749eb4e066b1900b881363817b
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-29 21:11:28 +0900

    Add Footer

---
+++ src/components/Common/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+import * as S from "./style"
+
+const Footer: FunctionComponent = () => {
+  return (
+    <S.FooterWrapper>
+      Thank You for Visiting My Blog, Have a Good Day 😆
+      <br />© 2021 Developer Kim, Powered By Gatsby.
+    </S.FooterWrapper>
+  )
+}
+
+export default Footer;
