4ca0f6f1e1f02f758ec74b781798d4cbfa2f40de
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 18:11:41 +0900

    Add PostList style

---
+++ src/components/Main/PostList/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
