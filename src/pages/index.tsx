d568bdc0214d18194a5d288774efaeb0d8b970c9
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 18:03:23 +0900

    Add CategoryList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,13 @@ import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
 import Introduction from 'components/Main/Introduction';
 import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
@@ -15,6 +22,7 @@ const IndexPage: FunctionComponent = () => {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory='Web' categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   )
