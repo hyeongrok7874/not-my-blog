282368cc79c844b012969895be6258a9fa9c9cb0
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 18:11:33 +0900

    Add PostList

---
+++ src/components/Main/PostList/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import * as S from "./style"
+
+const PostList: FunctionComponent = () => {
+  return (
+    <S.PostListWrapper>
+
+    </S.PostListWrapper>
+  )
+}
+
+export default PostList
