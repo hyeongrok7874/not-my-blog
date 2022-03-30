005007edad44103ce00fbf086b7e484bd760d17a
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 20:09:39 +0900

    Add media query

--- src/components/Main/CategoryList/style.ts
+++ src/components/Main/CategoryList/style.ts
@@ -17,6 +17,11 @@ export const CategoryListWrapper = styled.div`
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -31,5 +36,8 @@ export const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
 
   &:last-of-type {
     margin-right: 0;
+  }
+  @media (max-width: 768px) {
+    font-size: 15px;
   }
 `
