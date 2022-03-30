b1558191a6dbf6f071671109b726f2b81fc46c6c
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-31 08:37:18 +0900

    Update PostList

--- src/components/Main/PostList/index.tsx
+++ src/components/Main/PostList/index.tsx
@@ -2,23 +2,32 @@ import React, { FunctionComponent } from "react";
 import PostItem from "../PostItem";
 import * as S from "./style"
 
-const POST_ITEM_DATA = {
-  title: '게시글 제목',
-  date: '2022.03.30.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail:
-    'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mbp14-spacegray-select-202110_GEO_KR?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1647363032344',
-  link: 'https://github.com/hyeongrok7874/',
+export type PostType = {
+  node: {
+    id: string
+    frontmatter: {
+      title: string
+      summary: string
+      date: string
+      categories: string[]
+      thumbnail: {
+        publicURL: string
+      }
+    }
+  }
 }
 
-const PostList: FunctionComponent = () => {
+type PostListProps = {
+  posts: PostType[]
+}
+
+const PostList: FunctionComponent<PostListProps> = ({posts}) => {
   return (
     <S.PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
+      {posts.map(({ node: { id, frontmatter }}:PostType) => (
+        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+        )
+      )}
     </S.PostListWrapper>
   )
 }
