e9193685ee8e6bf449b9baa1f9522959e5d3f393
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 20:01:00 +0900

    Add postitem style

---
+++ src/components/Main/PostItem/style.ts
@@ -0,0 +1,78 @@
+import styled from "@emotion/styled";
+import { Link } from "gatsby";
+
+export const PostItemWrapper = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.15);
+  transition: 0.3s box-shadow;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+`
+
+export const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 200px;
+  border-radius: 10px 10px 0 0;
+  object-fit: cover;
+`
+
+export const PostItemContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`
+
+export const Title = styled.div`
+  display: -webkit-box;
+  overflow: hidden;
+  margin-bottom: 3px;
+  text-overflow: ellipsis;
+  white-space: normal;
+  overflow-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-size: 20px;
+  font-weight: 700;
+`
+
+export const Date = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  opacity: 0.7;
+`
+
+export const Category = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  margin: 10px -5px;
+`
+
+export const CategoryItem = styled.div`
+  margin: 2.5px 5px;
+  padding: 3px 5px;
+  border-radius: 3px;
+  background: black;
+  font-size: 14px;
+  font-weight: 700;
+  color: white;
+`
+
+export const Summary = styled.div`
+  display: -webkit-box;
+  overflow: hidden;
+  margin-top: auto;
+  text-overflow: ellipsis;
+  white-space: normal;
+  overflow-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-size: 16px;
+  opacity: 0.8;
+`
