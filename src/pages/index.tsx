9216705174956f298e20f4c4388c8923a93cd16f
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 18:11:26 +0900

    Add PostList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from 'components/Common/GlobalStyle';
 import Introduction from 'components/Main/Introduction';
 import Footer from 'components/Common/Footer';
 import CategoryList from 'components/Main/CategoryList';
+import PostList from 'components/Main/PostList';
 
 const CATEGORY_LIST = {
   All: 5,
@@ -23,6 +24,7 @@ const IndexPage: FunctionComponent = () => {
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory='Web' categoryList={CATEGORY_LIST} />
+      <PostList />
       <Footer />
     </Container>
   )
