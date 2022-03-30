313c333002b6fd7a1dc2aa56426f75d6fcdd0f1f
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-30 20:09:42 +0900

    Add media query

--- src/components/Main/Introduction/style.ts
+++ src/components/Main/Introduction/style.ts
@@ -14,15 +14,26 @@ export const Wrapper = styled.div`
   width: 768px;
   height: 400px;
   margin: 0 auto;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 300px;
+    padding: 0 20px;
+  }
 `
 
 export const SubTitle = styled.div`
   font-size: 20px;
   font-weight: 400;
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `
 
 export const Title = styled.div`
   margin-top: 5px;
   font-size: 35px;
   font-weight: 700;
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
 `
