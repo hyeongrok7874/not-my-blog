2502e7a1b58c78d72272aa51ecadc15fd3e5d669
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-01 11:41:59 +0900

    Update graphql query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,17 @@ import CategoryList from 'components/Main/CategoryList';
 import PostList from 'components/Main/PostList';
 import { graphql } from 'gatsby';
 import { PostListItemType } from 'types/PostItem.types';
+import {IGatsbyImageData} from 'gatsby-plugin-image'
 
 type IndexPageProps = {
   data: {
     allMarkdownRemark: {
       edges: PostListItemType[]
+    }
+    file: {
+      childImageSharp: {
+        gatsbyImageData: IGatsbyImageData
+      }
     }
   }
 }
@@ -31,12 +37,15 @@ const Container = styled.div`
 const IndexPage: FunctionComponent<IndexPageProps> = ({
   data: {
     allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+    },
   },
 }) => {
   return (
     <Container>
       <GlobalStyle />
-      <Introduction />
+      <Introduction profileImage={gatsbyImageData}/>
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <PostList posts={edges}/>
       <Footer />
@@ -60,10 +69,17 @@ export const getPostList = graphql`
             date(formatString: "YYYY.MM.DD.")
             categories
             thumbnail {
-              publicURL
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
             }
           }
         }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
       }
     }
   }
