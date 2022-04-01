3de511e75e785da87d39b6818758e52fd8e4b062
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-01 11:52:12 +0900

    Update introduction

--- src/components/Main/Introduction/index.tsx
+++ src/components/Main/Introduction/index.tsx
@@ -1,12 +1,19 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent} from "react";
 import ProfileImage from "../ProfileImage";
 import * as S from "./style"
+import { IGatsbyImageData } from "gatsby-plugin-image";
 
-const Introduction: FunctionComponent = () => {
+type IntroductionProps = {
+  profileImage: IGatsbyImageData
+}
+
+const Introduction: FunctionComponent<IntroductionProps> = ({
+  profileImage,
+}) => {
   return (
     <S.Background>
       <S.Wrapper>
-        <ProfileImage />
+        <ProfileImage profileImage={profileImage} />
         <div>
           <S.SubTitle>Nice to Meet You,</S.SubTitle>
           <S.Title>I'm Junior Frontend Developer Kim.</S.Title>
