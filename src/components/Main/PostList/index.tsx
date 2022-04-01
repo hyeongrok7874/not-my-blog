e375721794077c4dae333ef65dea5bd189ec6f6c
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-01 11:50:01 +0900

    Update PostList

--- src/components/Main/PostList/index.tsx
+++ src/components/Main/PostList/index.tsx
@@ -3,21 +3,6 @@ import PostItem from "../PostItem";
 import * as S from "./style"
 import { PostListItemType} from "types/PostItem.types"
 
-export type PostType = {
-  node: {
-    id: string
-    frontmatter: {
-      title: string
-      summary: string
-      date: string
-      categories: string[]
-      thumbnail: {
-        publicURL: string
-      }
-    }
-  }
-}
-
 type PostListProps = {
   posts: PostListItemType[]
 }
@@ -25,8 +10,8 @@ type PostListProps = {
 const PostList: FunctionComponent<PostListProps> = ({posts}) => {
   return (
     <S.PostListWrapper>
-      {posts.map(({ node: { id, frontmatter }}:PostType) => (
-        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+      {posts.map(({ node: { id, frontmatter }}:PostListItemType) => (
+          <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
         )
       )}
     </S.PostListWrapper>
