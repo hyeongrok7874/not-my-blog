da1ed55b6551256b3478dfec684c6cb44167730a
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-03 22:00:56 +0900

    Update PostList

--- src/components/Main/PostList/index.tsx
+++ src/components/Main/PostList/index.tsx
@@ -1,19 +1,29 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useMemo } from "react";
 import PostItem from "../PostItem";
 import * as S from "./style"
-import { PostListItemType} from "types/PostItem.types"
+import { PostListItemType } from "types/PostItem.types"
 
 type PostListProps = {
+  selectedCategory: string
   posts: PostListItemType[]
 }
 
-const PostList: FunctionComponent<PostListProps> = ({posts}) => {
+const PostList: FunctionComponent<PostListProps> = ({ selectedCategory, posts }) => {
+  const postListData = useMemo(
+    () => 
+      posts.filter(({ node: { frontmatter: { categories } } }: PostListItemType) => 
+        selectedCategory !== 'All'
+          ? categories.includes(selectedCategory)
+          : true,
+      ),
+    [selectedCategory]
+  )
+
   return (
     <S.PostListWrapper>
-      {posts.map(({ node: { id, frontmatter }}:PostListItemType) => (
-          <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
-        )
-      )}
+      {postListData.map(({ node: { id, frontmatter }}:PostListItemType) => (
+        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+      ))}
     </S.PostListWrapper>
   )
 }
