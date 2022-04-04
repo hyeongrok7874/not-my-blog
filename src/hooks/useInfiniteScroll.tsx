34f55b5ad425136aded634f792ceb09d9e750494
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-04 12:19:30 +0900

    Update useInfiniteScroll hook

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,13 +1,32 @@
-import { MutableRefObject, useRef } from "react";
+import { MutableRefObject, useMemo, useRef, useState } from "react";
 import { PostListItemType } from 'types/PostItem.types'
+
+export type useInfiniteScrollType = {
+  containerRef: MutableRefObject<HTMLDivElement | null>
+  postList: PostListItemType[]
+}
+
+const NUMBER_OF_ITEMS_PER_PAGE = 10
 
 const useInfiniteScroll = (selectedCategory: string, posts: PostListItemType[]) => {
   const containerRef: MutableRefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(
     null,
   )
+  const [count, setCount] = useState<number>(1)
+  
+  const postListByCategory = useMemo<PostListItemType[]>(
+    () =>
+      posts.filter(({ node: { frontmatter: { categories } } }: PostListItemType) =>
+        selectedCategory !== 'All'
+          ? categories.includes(selectedCategory)
+          : true,
+      ),
+    [selectedCategory],
+  )
 
   return {
-    containerRef
+    containerRef,
+    postList: postListByCategory.slice(0, count * NUMBER_OF_ITEMS_PER_PAGE),
   }
 }
 
