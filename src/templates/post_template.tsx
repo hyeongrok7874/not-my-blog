99aeb3c6ab4c7a22408beb76e953be49ca76d4f3
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-04 15:12:43 +0900

    Add template

---
+++ src/templates/post_template.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent} from "react";
+
+type PostTemplateProps = {}
+
+const PostTemplate: FunctionComponent<PostTemplateProps> = (props) => {
+  console.log(props)
+
+  return <>Post Template</>
+}
+
+export default PostTemplate
