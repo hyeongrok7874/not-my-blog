98d4308fd57af26ac515f371ca16c930b7a90fde
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-04 14:58:00 +0900

    Update graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,6 +90,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
