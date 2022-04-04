ec5a5d21604ec05f8c83c32e4b0ca60f7b3b3fba
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-04 13:40:21 +0900

    Update useInfiniteScroll hook

--- src/components/Main/PostList/index.tsx
+++ src/components/Main/PostList/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, useMemo } from "react";
 import PostItem from "../PostItem";
 import * as S from "./style"
 import { PostListItemType } from "types/PostItem.types"
-import useInfiniteScroll from "hooks/useInfiniteScroll"
+import useInfiniteScroll, {useInfiniteScrollType} from "hooks/useInfiniteScroll"
 
 type PostListProps = {
   selectedCategory: string
@@ -10,11 +10,11 @@ type PostListProps = {
 }
 
 const PostList: FunctionComponent<PostListProps> = ({ selectedCategory, posts }) => {
-  const { containRef } = useInfiniteScroll(selectedCategory, posts)
+  const { containRef, postList }: useInfiniteScrollType = useInfiniteScroll(selectedCategory, posts)
 
   return (
     <S.PostListWrapper ref={containRef}>
-      {posts.map(({ node: { id, frontmatter }}:PostListItemType) => (
+      {postList.map(({ node: { id, frontmatter }}:PostListItemType) => (
         <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
       ))}
     </S.PostListWrapper>
