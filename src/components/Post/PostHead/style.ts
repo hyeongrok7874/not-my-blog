4304aeb08b8091f01a5fad027af56531cd6c952d
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-06 17:55:51 +0900

    Update PostHead style

--- src/components/Post/PostHead/style.ts
+++ src/components/Post/PostHead/style.ts
@@ -11,6 +11,10 @@ export const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 export const BackgroundImage = styled((props: GatsbyImgProps) => (
@@ -21,4 +25,8 @@ export const BackgroundImage = styled((props: GatsbyImgProps) => (
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
