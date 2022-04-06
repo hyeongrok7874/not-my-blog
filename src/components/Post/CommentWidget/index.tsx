949bcb55a90fb0a94656b8391f24b9116aab6228
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-06 17:36:11 +0900

    Add CommentWidget

---
+++ src/components/Post/CommentWidget/index.tsx
@@ -0,0 +1,45 @@
+import React, { createRef, FunctionComponent, useEffect } from 'react'
+import * as S from "./style"
+
+const src = 'https://utteranc.es/client.js'
+const repo = 'hyeongrok7874/gatsby'
+
+type UtterancesAttributesType = {
+  src: string
+  repo: string
+  'issue-term': string
+  label: string
+  theme: string
+  crossorigin: string
+  async: string
+}
+
+const CommentWidget: FunctionComponent = () => {
+  const element = createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (element.current === null) return
+
+    const utterances: HTMLScriptElement = document.createElement('script')
+
+    const attributes: UtterancesAttributesType = {
+      src,
+      repo,
+      'issue-term': 'pathname',
+      label: 'Comment',
+      theme: `github-light`,
+      crossorigin: 'anonymous',
+      async: 'true',
+    }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value)
+    })
+
+    element.current.appendChild(utterances)
+  }, [])
+
+  return <S.UtterancesWrapper ref={element} />
+}
+
+export default CommentWidget
